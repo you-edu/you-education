e78add3e92d583636e48d587900e9964ad7467d6
Rohan Bhardwaj <rohanbhardwaj720@gmail.com>
2025-05-27 23:02:26 +0530

    extra part in google login removed

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -12,13 +12,6 @@ const handler = NextAuth({
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      authorization: {
-        params: {
-          prompt: "consent",
-          access_type: "offline",
-          response_type: "code"
-        }
-      }
     }),
     // ...add more providers here
   ],
