a426c8efbd8219536d232ca9bdb601ce2c3cbfa2
Rohan Bhardwaj <rohanbhardwaj720@gmail.com>
2025-05-28 23:00:25 +0530

    user api endpoints added

---
+++ src/app/api/user/route.tsx
@@ -0,0 +1,43 @@
+import { User } from '@lib/db/models';
+import { connectToDatabase } from '@lib/db/mongoose';
+import { NextResponse, NextRequest } from 'next/server';
+import {useSession} from 'next-auth/react';
+
+export async function GET(request: NextRequest) {
+    try {
+        const { data: session } = useSession();
+        console.log('Session data:', session);
+        const email = session?.user?.email;
+        const user = await User.findOne({email});
+
+        if(user){
+            // return user data as JSON with status 200
+            return NextResponse.json(user.toObject(), { status: 200, headers: { 'Content-Type': 'application/json' } });
+        }else{
+            return NextResponse.json({ error: 'User not found' }, { status: 404 });
+        }
+        
+    } catch (error) {
+        console.error('Error fetching users:', error);
+        return NextResponse.json({ error: 'Failed to fetch users' }, { status: 500 });
+    }
+}
+
+export async function POST(request: NextRequest) {
+    try {
+        const { name, email, image } = await request.json();
+        const newUser = new User({ name, email, image });
+        // check if user already exists
+        const existingUser = await User.findOne({ email });
+        if (existingUser) {
+            return NextResponse.json({ error: 'User already exists' }, { status: 400 });
+        }
+        
+        await newUser.save();
+        return NextResponse.json(newUser.toObject(), { status: 201 });
+    } catch (error) {
+        console.error('Error creating user:', error);
+        return NextResponse.json({ error: 'Failed to create user' }, { status: 500 });
+    }
+}
+connectToDatabase();
