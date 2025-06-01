27efac25d19a948395216bf77ba3b6437117cf08
Rohan Bhardwaj <rohanbhardwaj720@gmail.com>
2025-06-02 01:22:01 +0530

    post user end-point corrected

--- src/app/api/users/[userId]/route.tsx
+++ src/app/api/users/[userId]/route.tsx
@@ -23,23 +23,6 @@ export async function GET(request: NextRequest, {params} : {params: { userId: st
     }
 }
 
-export async function POST(request: NextRequest) {
-    try {
-        const { name, email, image } = await request.json();
-        const newUser = new User({ name, email, image });
-        // check if user already exists
-        const existingUser = await User.findOne({ email });
-        if (existingUser) {
-            return NextResponse.json({ error: 'User already exists' }, { status: 400 });
-        }
-        
-        await newUser.save();
-        return NextResponse.json(newUser.toObject(), { status: 201 });
-    } catch (error) {
-        console.error('Error creating user:', error);
-        return NextResponse.json({ error: 'Failed to create user' }, { status: 500 });
-    }
-}
 connectToDatabase()
   .then(() => console.log('Connected to MongoDB'))
   .catch((error) => console.error('MongoDB connection error:', error));
